e453e72926489eebb2e54b89dc7d41c1e11e0f19
Thiago Moura <tmoura1981@gmail.com>
2021-11-24 15:33:44 -0300

    Create Desafio016 - Mostrando só a parte inteira de um nº decimal. .py

---
+++ Desafio016 - Mostrando só a parte inteira de um nº decimal. .py
@@ -0,0 +1,7 @@
+n = float(input('Digite um nº decimal: '))
+print(f'O inteiro do nº {n} é {int(n)}.')
+
+'''ou 
+from math import floor
+= float(input('Digite um nº decimal: '))
+print('A porção inteira do nº {} é {}.' format(N, floor(N)))'''
