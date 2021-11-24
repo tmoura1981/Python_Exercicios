1041a1c244cb2d27c41c1d4f36c690cf3d743398
Thiago Moura <tmoura1981@gmail.com>
2021-11-24 15:32:48 -0300

    Create Desafio010 - Conversor de Moedas.py

---
+++ Desafio010 - Conversor de Moedas.py
@@ -0,0 +1,11 @@
+#Real x Dólar
+d = 5.46
+r = float(input('\033[1;32mQuantos reais você têm em sua carteira? R$ \033[m'))
+print(f'\033[1;32mVocê pode comprar até {r/d:.2f} US$.\033[m')
+
+
+
+
+
+
+
