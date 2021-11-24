73101cbe32ccdd088d49e923e5b9d741eefee23d
Thiago Moura <tmoura1981@gmail.com>
2021-11-24 15:32:42 -0300

    Create Desafio009 - Tabuada v1.py

---
+++ Desafio009 - Tabuada v1.py
@@ -0,0 +1,28 @@
+print('\033[1;33m-\033[m' * 50)
+a = int(input('\033[1;33mInforme um nº para calcular sua tabuada: \033[m'))
+print('\033[1;33m-\033[m' * 50)
+print(a, 'x 1 =', a * 1)
+print(a, 'x 2 =', a * 2)
+print(a, 'x 3 =', a * 3)
+print(a, 'x 4 =', a * 4)
+print(a, 'x 5 =', a * 5)
+print(a, 'x 6 =', a * 6)
+print(a, 'x 7 =', a * 7)
+print(a, 'x 8 =', a * 8)
+print(a, 'x 9 =', a * 9)
+print(a, 'x 10 =',a * 10)
+print('\033[1;33m-\033[m' * 50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
