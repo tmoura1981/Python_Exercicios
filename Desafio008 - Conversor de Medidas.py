777a0660da235a19a22b792b05c40dd130273165
Thiago Moura <tmoura1981@gmail.com>
2021-11-24 15:32:34 -0300

    Create Desafio008 - Conversor de Medidas.py

---
+++ Desafio008 - Conversor de Medidas.py
@@ -0,0 +1,5 @@
+print('\033[1;36m-\033[m' * 50)
+a = float(input('\033[1;36mDigite um valor em metros: \033[m'))
+print(f'\033[1;34mSeu valor em centímetros é de {a*100:.0f} \ne seu valor me milímetros é de {a*1000:.0f}\033[m')
+
+
