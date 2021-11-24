d8d050414b1260316a3ab55438456921291bef89
Thiago Moura <tmoura1981@gmail.com>
2021-11-24 15:33:28 -0300

    Create Desafio014 - Conversor de Temperaturas.py

---
+++ Desafio014 - Conversor de Temperaturas.py
@@ -0,0 +1,10 @@
+#Celsius x Fahrenheit
+c = float(input('\033[1;32mInforme a temperatura em ºC: \033[m'))
+f = (c * 9/5) + 32
+print(f'\033[1;32mA temperatura de {c} ºC \ntransformada em fahreinheit é de {f}Fº.\033[m')
+
+
+
+
+
+
