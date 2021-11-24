e9761ae394ffd486d6f6ca3eff3edfdbd8e3bd0a
Thiago Moura <tmoura1981@gmail.com>
2021-11-24 15:35:58 -0300

    Create Desafio023 - Separando Dígitos de um Número.py

---
+++ Desafio023 - Separando Dígitos de um Número.py
@@ -0,0 +1,11 @@
+numero = int(input('Digite um nº: '))
+unidade = numero // 1 % 10
+dezena = numero // 10 % 10
+centena = numero // 100 % 10
+milhar = numero // 1000 % 10
+print(f'Unidade: {unidade}')
+print(f'Dezena: {dezena}')
+print(f'Centena: {centena}')
+print(f'Milhar: {milhar}')
+
+
