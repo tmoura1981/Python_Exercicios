319c3bd4531c705bf4a5ef3dba6e634cef106feb
Thiago Moura <tmoura1981@gmail.com>
2021-11-24 15:37:28 -0300

    Create Desafio04 - Dissecando uma Variável.py

---
+++ Desafio04 - Dissecando uma Variável.py
@@ -0,0 +1,29 @@
+#Dissecando uma variável    
+a = (input("Digite algo: "))
+print('-' * 50)
+print("O tipo primitivo deste valor é",type(a))
+print("Só tem espaços?",a.isspace())
+print("É um número?",a.isnumeric())
+print("É alfabético?",a.isalpha())
+print("É alfanumérico?",a.isalnum())
+print("Está com letras maiúsculas?",a.isupper())
+print("Está com letras minúsculas?",a.islower())
+print("Está capitalizada?",a.istitle())
+print('-' * 50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
