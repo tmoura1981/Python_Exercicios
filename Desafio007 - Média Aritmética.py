873618fd8c74b8981bcfcc56df8696726c3d1c38
Thiago Moura <tmoura1981@gmail.com>
2021-11-24 15:32:08 -0300

    Create Desafio007 - Média Aritmética.py

---
+++ Desafio007 - Média Aritmética.py
@@ -0,0 +1,10 @@
+#Cálculo de 2 notas + a média
+n1 = float(input('\033[1;36mInforme a 1ª nota: \033[m'))
+n2 = float(input('\033[1;36mInforme a 2ª nota: \033[m'))
+s = (n1 + n2) / 2
+print(f'\033[1;32mA média entre {n1} e {n2} é {s}.\033[m')
+
+
+
+
+
