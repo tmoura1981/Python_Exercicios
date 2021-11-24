6ad399d8e755fd5e9a43c5f4e4b8f317bc774bea
Thiago Moura <tmoura1981@gmail.com>
2021-11-24 15:32:03 -0300

    Create Desafio006 - Triplo, dobro e raíz quadrada de um nº .py

---
+++ Desafio006 - Triplo, dobro e raíz quadrada de um nº .py
@@ -0,0 +1,10 @@
+a = int(input('Digite um nº '))
+print(f'Qual o seu dobro? ', a * 2)
+print(f'Qual o seu triplo?', a * 3)
+print(f'Qual a sua raíz quadrada? {a ** 0.5:.3f}')
+print(f'Qual a sua raíz cúbica? {a ** 0.33:.2f}')
+
+
+
+
+
